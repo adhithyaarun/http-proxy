d970cc8dafb3189ed066c81bd047a224593b94ae
adhithyaarun <adhithya.arun@students.iiit.ac.in>
2019-04-02 19:31:11 +0530

    Updated proxy server

--- proxy.py
+++ proxy.py
@@ -1,4 +1,6 @@
+'''Proxy Server'''
 import os
+import queue
 import socket
 import sys
 import threading
@@ -6,12 +8,15 @@ import time
 
 BUFFER_SIZE = 4096
 MAX_CONNECTION = 25
+ALLOWED_ACTIONS = ['GET', 'POST']
+CACHE_SIZE = 3
 
 PORT = 20100
 HOST = ''
 
 class Proxy:
     def __init__(self, port, hostname):
+        '''Constructor for Proxy Server'''
         try:
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             print('[log] Socket successfully created')
@@ -19,12 +24,15 @@ class Proxy:
             print('[log] Socket cration error: {}'.format(err))
         
         self.services = []
+        self.cache = queue.Queue(maxsize=CACHE_SIZE)
 
         # if os.path.isdir('./.cache'):
         #     os.makedirs('./.cache')
         #     print('[log] Cache created')
 
     def serverService(self):
+        '''Server end of the Proxy Server, 
+        that will handle requests from clients'''
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((HOST, PORT))
         self.server.listen(MAX_CONNECTION)
@@ -32,8 +40,7 @@ class Proxy:
         while True:
             try:
                 (conn, addr) = self.server.accept()
-                print('[log] Connection received from host address: {}'.format(addr))
-                # print(self.services)
+                print('[log] Connection received from host: {}, port: {}'.format(addr[0], addr[1]))
                 thread = threading.Thread(target=self.clientService, args=(conn, addr))
                 thread.start()
 
@@ -57,12 +64,33 @@ class Proxy:
         self.server.close()
 
     def clientService(self, conn, addr):
+        '''Client end of the Proxy Server, that will make requests to servers, 
+        get data and send to the client'''
+        if addr[1] < 20000 or addr[1] > 20099:
+            conn.send('Access denied.\n'.encode('utf-8'))
+            conn.close()
+            print('[log] Connection from host: {}, port: {} denied'.format(addr[0], addr[1]))
+            return
+
         try:
             # HTTP request
             request = str(conn.recv(BUFFER_SIZE))
 
-            (server, port, filename) = self.requestInfo(request)
-            print('Server: {}, Port: {}, File: {}'.format(server, port, filename))
+            (req_type, server, port, filename) = self.requestInfo(request)
+            # print('Request: {}, Server: {}, Port: {}, File: {}'.format(req_type, server, port, filename))
+
+            # Reject requests that are not GET or POST
+            if req_type not in ALLOWED_ACTIONS:
+                conn.send('Invalid action\n'.encode('utf-8'))
+                conn.close()
+                print('[log] {} request from host: {}, port: {} denied'.format(req_type, addr[0], addr[1]))
+                return
+            
+            if port > 20200 or port < 20101:
+                conn.send('Invalid action\n'.encode('utf-8'))
+                conn.close()
+                print('[log] Request from host: {}, port: {} to host: {}, port: {} denied'.format(addr[0], addr[1], server, port))
+                return
 
             # Connect to server
             try:
@@ -75,8 +103,8 @@ class Proxy:
                 return
             
             # Send request to server
-            if filename in os.listdir('./.cache'):
-                server_sock.send('GET /' + filename + ' HTTP/1.1\r\nIf-Modified-Since: ' + time.ctime(os.path.getmtime('./.cache/' + filename)) + ' \r\n\r\n')
+            if str(filename + server + port) in os.listdir('./.cache'):
+                server_sock.send('GET /' + filename + ' HTTP/1.1\r\nIf-Modified-Since: ' + time.ctime(os.path.getmtime('./.cache/' + str(filename + server + port))) + ' \r\n\r\n')
             else:
                 server_sock.send('GET /' + filename + 'HTTP/1.1\r\n\r\n')
 
@@ -85,9 +113,9 @@ class Proxy:
 
             print(response)
             
-            # Cache it
+            # Use cache, or update cache
             if response.find('200') != -1:
-                f = open(os.path.join('./.cache/', filename), 'wb')
+                f = open(os.path.join('./.cache/', str(filename + server + port)), 'wb')
                 while True:
                     if len(response) > 0:
                         f.write(response.encode('utf-8'))
@@ -99,26 +127,27 @@ class Proxy:
                         break
                 f.close()
             elif response.find('304') != -1:
-                # print('[log] Request serviced from cache \n\tfile: {}\n\tsize: {}'.format(filename, res_size))
-                f = open(os.path.join('./.cache/', filename), 'rb')
+                f = open(os.path.join('./.cache/', str(filename + server + port)), 'rb')
                 line = f.read(BUFFER_SIZE)
                 while line:
                     conn.send(line)
+                    # res_size = float(len(line) / 1024)
+                    # print('[log] Request serviced from cache \n\tfile: {}\n\tsize: {}'.format(filename, res_size))
                     line = f.read(BUFFER_SIZE)
                 f.close()
             else:
                 print('[log] Response from server: {}'.format(response))
 
-            # conn.send('Acknowledgement\n'.encode('utf-8'))
-
             server_sock.close()
-            conn.close()
+            # conn.send('Acknowledgement\n'.encode('utf-8'))
         except Exception as e:
             print('[log] Error: {}'.format(e))
             conn.send('Error in connecting to server, try again later\n'.encode('utf-8'))
         conn.close()
 
     def requestInfo(self, request):
+        '''Function to extract server, port and filename requested from HTTP request'''
+        req_type = request.split()[0][2:]
         url = request.split()[1]
         # line = request.split('\r\n')[0]
         # url = line.split()[1]
@@ -151,7 +180,7 @@ class Proxy:
         except IndexError:
             filename = '/'
 
-        return (server, port, filename)
+        return (req_type, server, port, filename)
             
 
 proxy = Proxy(PORT, HOST)
