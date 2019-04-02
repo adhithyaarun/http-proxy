919548b4c062e4d734d3fdec104c8fe6b6391d59
Meher Shashwat Nigam <meher.shashwat@students.iiit.ac.in>
2019-04-02 20:16:53 +0530

    lol

--- proxy.py
+++ proxy.py
@@ -14,6 +14,7 @@ CACHE_SIZE = 3
 PORT = 20100
 HOST = ''
 
+
 class Proxy:
     def __init__(self, port, hostname):
         '''Constructor for Proxy Server'''
@@ -22,7 +23,7 @@ class Proxy:
             print('[log] Socket successfully created')
         except socket.error as err:
             print('[log] Socket cration error: {}'.format(err))
-        
+
         self.services = []
         self.cache = queue.Queue(maxsize=CACHE_SIZE)
 
@@ -31,7 +32,7 @@ class Proxy:
         #     print('[log] Cache created')
 
     def serverService(self):
-        '''Server end of the Proxy Server, 
+        '''Server end of the Proxy Server,
         that will handle requests from clients'''
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((HOST, PORT))
@@ -40,14 +41,17 @@ class Proxy:
         while True:
             try:
                 (conn, addr) = self.server.accept()
-                print('[log] Connection received from host: {}, port: {}'.format(addr[0], addr[1]))
-                thread = threading.Thread(target=self.clientService, args=(conn, addr))
+                print('[log] Connection received from host: {}, port: {}'.format(
+                    addr[0], addr[1]))
+                thread = threading.Thread(
+                    target=self.clientService, args=(conn, addr))
                 thread.start()
 
                 for service in self.services:
                     if not service['thread'].isAlive():
                         service['connection'].close()
-                self.services = [service for service in self.services if service['thread'].isAlive()]
+                self.services = [
+                    service for service in self.services if service['thread'].isAlive()]
                 service = {
                     'thread': thread,
                     'connection': conn,
@@ -55,7 +59,7 @@ class Proxy:
                 }
                 self.services.append(service)
             except Exception as e:
-                print('[log] Error: {}'.format(e))
+                print('[log] Error: {}'.format(str(e)))
                 print('[log] Shutting down')
                 for service in self.services:
                     service['connection'].close()
@@ -64,12 +68,13 @@ class Proxy:
         self.server.close()
 
     def clientService(self, conn, addr):
-        '''Client end of the Proxy Server, that will make requests to servers, 
+        '''Client end of the Proxy Server, that will make requests to servers,
         get data and send to the client'''
         if addr[1] < 20000 or addr[1] > 20099:
             conn.send('Access denied.\n'.encode('utf-8'))
             conn.close()
-            print('[log] Connection from host: {}, port: {} denied'.format(addr[0], addr[1]))
+            print('[log] Connection from host: {}, port: {} denied'.format(
+                addr[0], addr[1]))
             return
 
         try:
@@ -83,28 +88,31 @@ class Proxy:
             if req_type not in ALLOWED_ACTIONS:
                 conn.send('Invalid action\n'.encode('utf-8'))
                 conn.close()
-                print('[log] {} request from host: {}, port: {} denied'.format(req_type, addr[0], addr[1]))
+                print('[log] {} request from host: {}, port: {} denied'.format(
+                    req_type, addr[0], addr[1]))
                 return
-            
+
             if port > 20200 or port < 20101:
                 conn.send('Invalid action\n'.encode('utf-8'))
                 conn.close()
-                print('[log] Request from host: {}, port: {} to host: {}, port: {} denied'.format(addr[0], addr[1], server, port))
+                print('[log] Request from host: {}, port: {} to host: {}, port: {} denied'.format(
+                    addr[0], addr[1], server, port))
                 return
 
             # Connect to server
             try:
                 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                server_sock.connect((server, port))            
+                server_sock.connect((server, port))
             except Exception as e:
-                print('[log] Error: {}'.format(e))
+                print('[log] Error: {}'.format(str(e)))
                 conn.send('Unable to connect with server\n'.encode('utf-8'))
                 conn.close()
                 return
-            
+
             # Send request to server
-            if str(filename + server + port) in os.listdir('./.cache'):
-                server_sock.send('GET /' + filename + ' HTTP/1.1\r\nIf-Modified-Since: ' + time.ctime(os.path.getmtime('./.cache/' + str(filename + server + port))) + ' \r\n\r\n')
+            if str(filename + server + str(port)) in os.listdir('./.cache'):
+                server_sock.send('GET /' + filename + ' HTTP/1.1\r\nIf-Modified-Since: ' + time.ctime(
+                    os.path.getmtime('./.cache/' + str(filename + server + str(port)))) + ' \r\n\r\n')
             else:
                 server_sock.send('GET /' + filename + 'HTTP/1.1\r\n\r\n')
 
@@ -112,10 +120,11 @@ class Proxy:
             response = str(server_sock.recv(BUFFER_SIZE))
 
             print(response)
-            
+
             # Use cache, or update cache
             if response.find('200') != -1:
-                f = open(os.path.join('./.cache/', str(filename + server + port)), 'wb')
+                f = open(os.path.join(
+                    './.cache/', str(filename + server + port)), 'wb')
                 while True:
                     if len(response) > 0:
                         f.write(response.encode('utf-8'))
@@ -127,7 +136,8 @@ class Proxy:
                         break
                 f.close()
             elif response.find('304') != -1:
-                f = open(os.path.join('./.cache/', str(filename + server + port)), 'rb')
+                f = open(os.path.join(
+                    './.cache/', str(filename + server + port)), 'rb')
                 line = f.read(BUFFER_SIZE)
                 while line:
                     conn.send(line)
@@ -141,9 +151,10 @@ class Proxy:
             server_sock.close()
             # conn.send('Acknowledgement\n'.encode('utf-8'))
         except Exception as e:
-            print('[log] Error: {}'.format(e))
-            conn.send('Error in connecting to server, try again later\n'.encode('utf-8'))
-        conn.close()
+            print('[log] Error: {}'.format(str(e)))
+            conn.send(
+                'Error in connecting to server, try again later\n'.encode('utf-8'))
+            conn.close()
 
     def requestInfo(self, request):
         '''Function to extract server, port and filename requested from HTTP request'''
@@ -153,7 +164,7 @@ class Proxy:
         # url = line.split()[1]
 
         no_protocol = url.find('://')
-        
+
         if no_protocol == -1:
             host_url = url
         else:
@@ -172,16 +183,17 @@ class Proxy:
             port = 20101
             server = host_url[:port_end]
         else:
-            port = int((host_url[(port_start + 1):])[:port_end - port_start - 1])
+            port = int((host_url[(port_start + 1):])
+                       [:port_end - port_start - 1])
             server = host_url[:port_start]
-        
+
         try:
             filename = host_url.split('/')[1]
         except IndexError:
             filename = '/'
 
         return (req_type, server, port, filename)
-            
+
 
 proxy = Proxy(PORT, HOST)
 if proxy.server:
